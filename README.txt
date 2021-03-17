9c589aadf5c53231d31c9f07bd75eca741552556
thanar <thanarnopeq@gmail.com>
2021-03-17 17:28:56 +0700

    t

---
+++ README.txt
@@ -0,0 +1,4 @@
+##Create by thanarnop Robkwaen##
+  **** How to test running *** 
+1 Run command : python TCPServer.py
+2 Run command : python test.py 
--- test.py
+++ test.py
@@ -8,7 +8,7 @@ app.start() # Start connection and running forever
 time.sleep(5) # Delay 5 secs
 app.send("test1") # Send message
 print(app.rcvmsg)
-time.sleep(2) 
-print(app.rcvmsg)# Delay 2 secs
-time.sleep(1) 
+time.sleep(2) # Delay 2 secs
+print(app.rcvmsg)# Print out to see receive message
+time.sleep(1) # Delay 2 secs
 app.stop() # Stop running 
