56b8ecfefd1bf450df990338420ed083b14a2d60
thanar <thanarnopeq@gmail.com>
2021-03-17 16:16:25 +0700

    first

---
+++ tcp.py
@@ -0,0 +1,44 @@
+import socket
+import signal
+import sys
+from threading import Thread
+
+class TCP():
+   
+    def __init__(self): 
+        self.running = True
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.address = ('0.0.0.0',5000)
+        self.th = Thread(target=self.receive)
+
+    def connect(self):
+        self.sock.connect(self.address)
+        print('connecting to =>',self.address)
+        
+    def send(self,message):
+        msg = bytes(message,'utf-8')
+        print('sending =>',message)
+        self.sock.sendall(msg)
+
+    def disconnect(self):
+        print("disconnect")
+        self.running = False
+        self.sock.close()
+
+    def receive(self):
+        self.connect()
+        while self.running:
+            data = self.sock.recv(16)
+            if len(data) > 15:
+                print('received "%s"',data.decode('utf-8'))
+            if self.running == False:
+                break
+
+    def start(self):
+        self.th.start()
+            
+                
+
+
+
+    
---
+++ test.py
@@ -0,0 +1,12 @@
+from tcp import TCP
+import time
+from threading import Thread
+
+
+app = TCP()
+app.address = ('192.168.250.1',5000)
+app.start()
+time.sleep(5)
+app.send("test1")
+time.sleep(2)
+app.disconnect()
